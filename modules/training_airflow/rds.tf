35f642dc38d79ce60209c6e4dc0e9fb70bcb6636
kaleeaswari <skaleeas@thoughtworks.com>
2019-11-26 14:20:12 +0530

    #22 | [Nagarajan/Kales] | Create RDS with username and password instead of creating it from Snapshot

--- modules/training_airflow/rds.tf
+++ modules/training_airflow/rds.tf
@@ -1,7 +1,16 @@
+resource "random_password" "password" {
+  length = 16
+  special = true
+  override_special = "_%@"
+}
+
 resource "aws_db_instance" "airflow_postgres" {
   identifier = "training-airflow-${var.deployment_identifier}"
 
-  snapshot_identifier    = "${var.initial_rds_snapshot}"
+  allocated-storage      = "30"
+  engine                 = "postgres"
+  username               = "airflow"
+  password               = "${random_password.password.result}"
   instance_class         = "${var.rds_instance_class}"
   vpc_security_group_ids = ["${aws_security_group.airflow_rds.id}"]
   db_subnet_group_name   = "${aws_db_subnet_group.airflow_postgres.name}"
@@ -11,6 +20,20 @@ resource "aws_db_instance" "airflow_postgres" {
       "Name", "training-airflow-${var.deployment_identifier}"
     )
   )}"
+}
+
+resource "aws_ssm_parameter" "secret" {
+  name        = "airflow-rds-password"
+  description = "RDS password for airflow user"
+  type        = "SecureString"
+  value       = "${random_password.password.result}"
+
+  tags = "${merge(
+      local.common_tags,
+      map(
+        "Name", "training-airflow-${var.deployment_identifier}"
+      )
+    )}"
 }
 
 resource "aws_db_subnet_group" "airflow_postgres" {
