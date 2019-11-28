21e80b7e891a687f072924f3afacaa752aea32d7
Sanjeev Upreti <sanjeevupreti@ip-10-10-5-226.ap-southeast-1.compute.internal>
2019-11-28 11:10:08 +0530

    [Nagarajan/Sanjeev] [#3] Set log retention period to 8 hours in kafka image

--- images/training_kafka/scripts/install_kafka.sh
+++ images/training_kafka/scripts/install_kafka.sh
@@ -22,6 +22,8 @@ EOF
 # Install Confluent platform
 sudo yum clean all && sudo yum install -y confluent-platform-oss-${CONFLUENT_SCALA_VERSION}
 
+sudo sed -i 's/log.retention.hours=168/log.retention.hours=8/g' /etc/kafka/server.properties
+
 sudo systemctl enable confluent-zookeeper
 sudo systemctl enable confluent-kafka
 
