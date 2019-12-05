267d565c694ea0da666b427b7f846ea9ef9538a4
Sanjeev Upreti <sanjeevupreti@gmail.com>
2019-12-05 15:08:56 +0530

    [Dong Yu/Sanjeev] [#11] set alarms in cloud watch

--- modules/monitoring/alarm.tf
+++ modules/monitoring/alarm.tf
@@ -25,16 +25,52 @@ EOF
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "delivery_file_check" {
+resource "aws_cloudwatch_metric_alarm" "has_station_mart_updated" {
   alarm_name                = "2wheelers-delivery-file-check-${var.cohort}"
-  comparison_operator       = "LessThanThreshold"
+  comparison_operator       = "MoreThanThreshold"
   evaluation_periods        = "1"
-  metric_name               = "delivery_file"
+  metric_name               = "has_station_mart_updated"
   namespace                 = "TwoWheelers"
   period                    = "300"
   statistic                 = "Average"
-  threshold                 = "1"
+  threshold                 = "5"
   alarm_description         = "This metric monitors for latest delivery file"
+  alarm_actions             = [ "${aws_sns_topic.alarm.arn}" ]
+
+  dimensions {
+    source = "airflow"
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "duplicate_station_id_count" {
+  alarm_name                = "2wheelers-duplicate_station_id_count-${var.cohort}"
+  comparison_operator       = "MoreThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "duplicate_station_id_count"
+  namespace                 = "TwoWheelers"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors for duplicate station count"
+  alarm_actions             = [ "${aws_sns_topic.alarm.arn}" ]
+
+  dimensions {
+    source = "airflow"
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "null_latitude_longitude_count" {
+  alarm_name                = "2wheelers-null_latitude_longitude_count-${var.cohort}"
+  comparison_operator       = "MoreThanThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "null_latitude_longitude_count"
+  namespace                 = "TwoWheelers"
+  period                    = "300"
+  statistic                 = "Average"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors for duplicate station count"
   alarm_actions             = [ "${aws_sns_topic.alarm.arn}" ]
 
   dimensions {
